4eb4f5a1519631b3e327e53b8068b1755f391d84
potterhe <potter.he@gmail.com>
2019-07-08 00:05:41 +0800

    CURD

--- src/main/scala/com/company/HBaseApp.scala
+++ src/main/scala/com/company/HBaseApp.scala
@@ -35,6 +35,53 @@ object HBaseApp {
       admin.createTable(tableDescr)
       println("Table `wetag` created  ")
     }
+
+    // 插入(更新)记录
+    val key="12345678"
+    val family="attr"
+    val column="population.age"
+    val value="30"
+    val table=conn.getTable(wetagTable)
+
+    var p=new Put(key.getBytes)
+    p.addColumn(family.getBytes,column.getBytes,value.getBytes())
+    p.addColumn(family.getBytes, "population.sex".getBytes, "male".getBytes)
+    table.put(p)
+
+    p=new Put(key.getBytes)
+    p.addColumn(family.getBytes, "population.age".getBytes, "45".getBytes)
+    table.put(p)
+
+    // 查询某条记录
+    val g = new Get(key.getBytes)
+    val result = table.get(g)
+    val gvalue = Bytes.toString(result.getValue(family.getBytes, column.getBytes))
+    println("GET: " + key + " " + gvalue)
+
+    // 删除记录
+    val d=new Delete(key.getBytes)
+    d.addColumn(family.getBytes(), "population.sex".getBytes())
+    table.delete(d)
+    println("Delete: " + key + "sex")
+
+    // 扫描记录
+    var scanner:ResultScanner=null
+    try{
+      val s=new Scan()
+      s.addFamily(family.getBytes())
+      scanner = table.getScanner(s)
+
+      var result:Result=scanner.next()
+      while(result!=null) {
+        println("Found row:" + result)
+        println("Found value: "+Bytes.toString(result.getValue(family.getBytes(),column.getBytes())))
+        result=scanner.next()
+      }
+    } finally {
+      if(table!=null)
+        table.close()
+      scanner.close()
+    }
   }
 
 }
