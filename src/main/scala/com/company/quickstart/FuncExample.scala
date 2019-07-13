dbf302b1e8ac73a0b1f73cae553edbac28d61980
potterhe <potter.he@gmail.com>
2019-07-13 15:46:08 +0800

    FuncExample
    fix

--- src/main/scala/com/company/quickstart/FuncExample.scala
+++ src/main/scala/com/company/quickstart/FuncExample.scala
@@ -1,8 +1,98 @@
 package com.company.quickstart
 
 object FuncExample {
+
+  def greet(): Unit = {
+    println("greet")
+  }
+
   def counter(v: Int): Int = {
     v + 1
+  }
+
+  /**
+    * function literal
+    * 函数字面量的语法是：用圆括号括起来的一组带名字的参数，一个右箭头和函数体
+    */
+  val fnMax = (a:Int, b:Int) => {
+    if (a > b)
+      a
+    else
+      b
+  }
+
+  /**
+    * 函数类型字面量语法
+    */
+  val fnMax2: (Int, Int) => Int = (a:Int, b:Int) => {
+    if (a > b)
+      a
+    else
+      b
+  }
+
+  /**
+    * 重复参数,可变长参数列表
+    *
+    * @param args
+    */
+  def echoString(args: String*):Unit = {
+    for (arg <- args) {
+      println(arg)
+    }
+
+    args.foreach(a => println(a))
+    args.foreach(println)
+  }
+
+  /**
+    * 重复参数,可变长参数列表
+    *
+    * @param args
+    */
+  def echoAny(args: Any*) = {
+    args.foreach(println)
+  }
+
+  /**
+    * 函数类型表示
+    *
+    * @param a
+    * @param fn
+    */
+  def arrayWalk(a: Array[Any], fn:(Any) => Unit): Unit = {
+    a.foreach(fn)
+  }
+
+  /**
+    * 函数默认值
+    *
+    * @param distance
+    * @param time
+    * @return
+    */
+  def speed(distance: Int, time: Int = 2): Int= {
+    distance / time
+  }
+
+}
+
+object TailRecurisive {
+  /**
+    * 尾递归(tail recursive): 在最后一步调用自己的函数.
+    * [Scala编译8.9]
+    */
+  def approximate(guess: Double): Double = {
+    if (isGoodEnough(guess)) guess
+    else approximate(improve(guess))
+  }
+
+  def isGoodEnough(guess: Double): Boolean = {
+    guess > 0.5
+  }
+
+  def improve(guess: Double): Double = {
+    guess + 0.1
   }
 
 }
--- src/test/scala/com/company/quickstart/FuncExampleTest.scala
+++ src/test/scala/com/company/quickstart/FuncExampleTest.scala
@@ -7,11 +7,62 @@ class FuncExampleTest {
   @Test
   def testCounter() = {
     val n = FuncExample.counter(1)
-    assertTrue(n == 2)
+    assertEquals(n, 2)
   }
 
-  def dataProviderCounter (): Unit = {
+  @Test
+  def testFuncMax() = {
+    val n = FuncExample.fnMax(1, 2)
+    assertEquals(n, 2)
 
+    val m = FuncExample.fnMax2(1, 2)
+    assertEquals(m, 2)
+  }
+
+  @Test
+  def testEchoString():Unit = {
+    FuncExample.echoString("a", "b", "c")
+
+    /**
+      * 将一个定义好的数组作为重复参数传入时，需在数组实参的后面加上冒号和"_*"符号，
+      * 以告诉编译器将arr的每个元素作为参数,而不是将所有元素放在一起作为单个实参传入.
+      */
+    val arr = Array("d", "e")
+    //FuncExample.echoString(arr)
+    FuncExample.echoString(arr:_*)
+
+    FuncExample.echoAny("a", 1, 1.0)
+  }
+
+  @Test
+  def testArrayWalk(): Unit = {
+    val arr:Array[Any] = Array("d", "e")
+    FuncExample.arrayWalk(arr, println)
+    FuncExample.arrayWalk(arr,
+      x => {
+        println("WALK:" + x)
+      }
+    )
+  }
+
+  @Test
+  def testNamedArgs(): Unit = {
+    val d = 100
+    val t = 10
+    val s = FuncExample.speed(d, t)
+    assertEquals(s, 10)
+
+    val s2 = FuncExample.speed(time = 10, distance = 100)
+    assertEquals(s2, s)
+
+    val s3 = FuncExample.speed(100)
+    assertEquals(s3, 50)
+  }
+
+  @Test
+  def testTailRecursive(): Unit ={
+    val r = TailRecurisive.approximate(0)
+    assertTrue(r > 0.5)
   }
 
 }
