cf326a707dad5f7104be755b5b8295228a5c0365
potterhe <potter.he@gmail.com>
2022-02-13 18:51:21 +0800

    LearnDuration with MyUDWF

--- src/main/scala/com/company/spark/LearnDuration.scala
+++ src/main/scala/com/company/spark/LearnDuration.scala
@@ -1,7 +1,11 @@
 package com.company.spark
 
 import org.apache.spark.sql.SparkSession
-
+import org.apache.spark.sql.catalyst.dsl.expressions.StringToAttributeConversionHelper
+import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.{functions => f}
+import org.apache.spark.sql.functions.{expr, lit}
+import org.apache.spark.sql.types.{LongType, StringType}
 /*
 spark-submit \
   --class "com.company.spark.LearnDuration" \
@@ -35,6 +39,18 @@ object LearnDuration {
         |FROM event_view
         |GROUP BY event
         |""".stripMargin).show()
+
+    val df2 = dfSensorEvent
+      //.withColumn("col_a", lit(1))
+      //.withColumn("user", expr("login_id"))
+      .withColumn("ts", expr("time * 1000").cast(LongType))
+      .withColumn("pk", f.concat(f.col("login_id"), lit("_"), f.col("time")))
+      .withColumn("session", expr("""IF(event == 'finish', pk, NULL)""").cast(StringType))
+    df2.printSchema()
+    df2.show()
+    val specs = Window.partitionBy(f.col("login_id")).orderBy(f.col("time").desc)
+    val df3 = df2.withColumn( "newsession", MyUDWF.calculateSession(f.col("ts"), f.col("session")) over specs)
+    df3.orderBy("time").show()
 
     spark.stop()
   }
